68edb8469e80e9e513787fdd0588a0dcfb6c0cc0
ahmed-seghairoon <ahmmedmohammed912@gmail.com>
2024-06-08 18:05:34 +0300

    started library app and setup home page

--- karabuk_university_library_system/settings.py
+++ karabuk_university_library_system/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'identity',
+    'library'
 ]
 
 MIDDLEWARE = [
--- karabuk_university_library_system/urls.py
+++ karabuk_university_library_system/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 admin.site.site_header = 'Karabuk University Library Management System'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('library.urls')),
 ]
